4744f3f0c6d4f44ad25a058e7b04006ab7115a29
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-02-14 20:23:55 +0100

    test: :white_check_mark: Total Row Count in Csv File

--- internal/csv/csv_test.go
+++ internal/csv/csv_test.go
@@ -1,6 +1,7 @@
 package csv_test
 
 import (
+	"encoding/csv"
 	"os"
 	"testing"
 
@@ -23,5 +24,37 @@ func TestOpenFileCsv(t *testing.T) {
 	if openFile == nil {
 		t.Errorf("Expected openFile to be not nil")
 	}
+
+}
+
+func TestTotalRowCountInCsvFile(t *testing.T) {
+	file, err := file.NewFilePath("../../file/test.csv")
+	if err != nil {
+		t.Errorf("Expected no error but got %s", err)
+	}
+
+	openFile, err := os.Open(file.Path)
+	if err != nil {
+		t.Errorf("Expected no error but got %s", err)
+	}
+
+	defer openFile.Close()
+
+	if openFile == nil {
+		t.Errorf("Expected openFile to be not nil")
+	}
+	//ReadCsvFile
+	fileData, err := csv.NewReader(openFile).ReadAll()
+	if err != nil {
+		t.Errorf("Expected no error but got %s", err)
+	}
+
+	//TotalRowCountInCsvFile
+	totalRowCount := len(fileData)
+	if totalRowCount == 0 {
+		t.Errorf("Expected totalRowCount to be 3 but got %d", totalRowCount)
+	}
+
+	t.Log("Total Row Count: ", totalRowCount)
 
 }
